6e30bcaea594f73a90b67fee6ef799b10481ea69
KhaiTQ10 <khaiquangtranabc@gmail.com>
2024-09-16 03:16:21 +0700

    update ShootAngle.py

--- ShootAngle.py
+++ ShootAngle.py
@@ -2,91 +2,91 @@ import math
 
 class Coordinate:
     def __init__(self, x : int, y : int):
-        self.mX = x
-        self.mY = y
+        self.__mX = x
+        self.__mY = y
 
     @property
     def x(self):
-        return self.mX
+        return self.__mX
     @x.setter
     def x(self, value):
-        self.mX = value
+        self.__mX = value
 
     @property
     def y(self):
-        return self.mY
+        return self.__mY
     @y.setter
     def y(self, value):
-        self.mY = value
+        self.__mY = value
 
     def __repr__(self):
-        return f"{self.__class__.__name__} ('x {self.mX} y {self.mY}')"
+        return f"{self.__class__.__name__} ('x {self.x} y {self.y}')"
 
-class ShootAngle:
-    mVo = 850 # uint (pixel/s)
-    mG = 600 # uint (pixel/s^2)
-    mX1 = 0.0
-    mX2 = 0.0
+class Distance:
     def __init__(self, point1 : Coordinate, point2 : Coordinate):
-        self.mPoint1 = point1
-        self.mPoint2 = point2
-
-    def __repr__(self):
-        return f"{self.__class__.__name__} ['point1 {self.mPoint1}, point2 {self.mPoint2}, \
-angle {round(self.calculateAngle, 2)} degrees, time {self.convertAngleToTime} s']"
-
-    @property
-    def point1(self):
-        return self.mPoint1
-    @property
-    def point2(self):
-        return self.mPoint2
-
-    @point1.setter
-    def point1(self, value : Coordinate):
-        self.mPoint1 = value
-    @point2.setter
-    def point2(self, value : Coordinate):
-        self.mPoint2 = value
+        self.__mPoint1 = point1
+        self.__mPoint2 = point2
 
     # find delta of 2 point in Coordinate system
     @property
     def deltaPointX(self):
-        return abs(self.mPoint1.mX - self.mPoint2.mX)
+        return abs(self.__mPoint1.x - self.__mPoint2.x)
     @property
     def deltaPointY(self):
-        return abs(self.mPoint1.mY - self.mPoint2.mY)
-
-    # solve quadratic equation
-    def calculateAParameter(self):
-        return (-0.5 * self.mG * pow(self.deltaPointX, 2)) / pow(self.mVo, 2)
-    def calculateBParameter(self):
-        return self.deltaPointX
-    def calculateCParameter(self):
-        return (-0.5 * self.mG * pow(self.deltaPointX, 2)) / pow(self.mVo, 2) - self.deltaPointY
+        return abs(self.__mPoint1.y - self.__mPoint2.y)
 
     @property
-    def calculateAngle(self):
-        a = self.calculateAParameter()
-        b = self.calculateBParameter()
-        c = self.calculateCParameter()
+    def point1(self):
+        return self.__mPoint1
+    @property
+    def point2(self):
+        return self.__mPoint2
+
+    @property
+    def distance(self):
+        return math.sqrt(pow(self.deltaPointX, 2) + pow(self.deltaPointY, 2))
+
+class ShootAngle:
+    __mVo = 850 # uint (pixel/s)
+    __mG = 600 # uint (pixel/s^2)
+    __mX1 = 0.0
+    __mX2 = 0.0
+    def __init__(self, point1 : Coordinate, point2 : Coordinate):
+        self.__mPoint1and2 = Distance(point1 , point2)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} ['point1 {self.__mPoint1and2.point1}, point2 {self.__mPoint1and2.point2}, \
+angle {round(self.calculateAngle, 2)} degrees, time {self.convertAngleToTime} s']"
+
+    # solve quadratic equation
+    def __calculateAParameter(self):
+        return (-0.5 * self.__mG * pow(self.__mPoint1and2.deltaPointX, 2)) / pow(self.__mVo, 2)
+    def __calculateBParameter(self):
+        return self.__mPoint1and2.deltaPointX
+    def __calculateCParameter(self):
+        return (-0.5 * self.__mG * pow(self.__mPoint1and2.deltaPointX, 2)) / pow(self.__mVo, 2) - self.__mPoint1and2.deltaPointY
+
+    def __calculateAngle(self):
+        a = self.__calculateAParameter()
+        b = self.__calculateBParameter()
+        c = self.__calculateCParameter()
 
         delta = b*b - 4*a*c
         assert delta >= 0 , print(f"equation with no solution. delta = {delta}")
 
-        self.mX1 = (-b + math.sqrt(delta)) / (2*a)
-        self.mX2 = (-b - math.sqrt(delta)) / (2*a)
+        self.__mX1 = (-b + math.sqrt(delta)) / (2*a)
+        self.__mX2 = (-b - math.sqrt(delta)) / (2*a)
 
         #find arctan
-        self.mX1 = math.atan(self.mX1)
-        self.mX2 = math.atan(self.mX2)
+        self.__mX1 = math.atan(self.__mX1)
+        self.__mX2 = math.atan(self.__mX2)
 
-        if self.mX1 >= 0:
-            self.mX1 = math.degrees(self.mX1)
-            return self.mX1
-        elif self.mX2 >= 0 :
-            self.mX2 = math.degrees(self.mX2)
-            return self.mX2
+        if self.__mX1 >= 0:
+            self.__mX1 = math.degrees(self.__mX1)
+            return self.__mX1
+        elif self.__mX2 >= 0 :
+            self.__mX2 = math.degrees(self.__mX2)
+            return self.__mX2
         else:
             print("calculate Angle is incorrect!!!")
             return 0
@@ -94,6 +94,16 @@ angle {round(self.calculateAngle, 2)} degrees, time {self.convertAngleToTime} s'
     # convert Angle to Shot time
     @property
     def convertAngleToTime(self):
-        assert self.calculateAngle > 0 , print(f"Angle is invalid.")
-        time = self.calculateAngle/90
+        assert self.__calculateAngle() > 0 , print(f"Angle is invalid.")
+        time = self.__calculateAngle()/90
         return round(time,2)
+
+    @property
+    def getDistance(self):
+        return self.__mPoint1and2.distance
+
+
+p1 = Coordinate(1, 2)
+p2 = Coordinate(3, 4)
+p3 = ShootAngle(p1, p2)
+print(p3.getDistance)
