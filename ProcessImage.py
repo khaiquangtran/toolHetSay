2a0d6b722124290712268550a6ddcab1e3357478
KhaiTQ10 <khaiquangtranabc@gmail.com>
2024-09-18 11:18:40 +0700

    update 18-09-2024

--- ProcessImage.py
+++ ProcessImage.py
@@ -6,11 +6,16 @@ import math
 from ShootAngle import Coordinate
 
 class ProcessImage:
-    __mThreshold = 0.5  # accuracy is 0.0
+    __mThreshold = 0.8  # accuracy is 0.0
     __mShooterPath = "./imageTarget/shooter.png"
     __mPersonShotPath = "./imageTarget/personshot.png"
     __mImageInput = None
-
+    __mShooterTopLeftPosition = None
+    __mPersonTopLeftPosition = None
+    __mShieldTopLeftPosition = None
+    __mBallTopLeftPosition = None
+    __mBallShootTopLeftPosition =None
+    __mWaterfallTopLeftPosition = None
     def __init__(self):
         self.__mImageShooter = cv2.imread(self.__mShooterPath)
         assert self.__mImageShooter is not None, print(f"Can't open shooter.png")
@@ -21,8 +26,15 @@ class ProcessImage:
         assert self.__mImagePersonShoot is not None, print(f"Can't open shooter.png")
         self.__mHeightPersonShot = self.__mImagePersonShoot.shape[0]
         self.__mWidthPersonShot = self.__mImagePersonShoot.shape[1]
+        # print("height ", self.__mHeightPersonShot)
+        # print("width ", self.__mWidthPersonShot)
 
         self.__mHeithScreenLaptop = pyautogui.size()[1]
+
+    def showImage(self, image):
+        cv2.imshow("Parabola on Image", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     def checkImageTarget(self, imageInput):
         # The methods for comparison
@@ -32,9 +44,9 @@ class ProcessImage:
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
         # Compare max value with threshold
         if max_val >= self.__mThreshold:
-            return (True, max_loc)
+            return max_loc
         else:
-            return (False, max_loc)
+            return 0
 
     def storeImage(self, imageStore):
         timeFormat = time.strftime("%Y%m%d_%H%M%S", time.localtime())
@@ -43,90 +55,67 @@ class ProcessImage:
         cv2.imwrite(direction, imageStore)
 
     def isBall(self):
-        imageBall = cv2.imread('./imageTarget/ball.png')
-        if imageBall is None:
-            print("Can't open ball image")
-            return False
-        res = self.checkImageTarget(imageBall)
-        if res[0] == True:
-            print("Find out ball")
-            return True
-        else:
+        if self.__mBallTopLeftPosition is None or self.__mBallTopLeftPosition == 0:
             print("Don't find out ball")
             return False
+        else:
+            print("Find out ball")
+            return True
 
     def isShield(self):
-        imageShield = cv2.imread('./imageTarget/shield.png')
-        if imageShield is None:
-            print("Can't open shield image")
-            return False
-        res = self.checkImageTarget(imageShield)
-        if res[0] == True:
-            print("Find out shield")
-            return True
-        else:
+        if self.__mShieldTopLeftPosition is None or self.__mShieldTopLeftPosition == 0:
             print("Don't find out shield")
             return False
+        else:
+            print("Find out shield")
+            return True
 
     def isBallShoot(self):
-        imageBallShoot = cv2.imread('./imageTarget/ballShoot.png')
-        if imageBallShoot is None:
-            print("Can't open ballShoot image")
-            return False
-        res = self.checkImageTarget(imageBallShoot)
-        if res[0] == True:
-            print("Find out ball shoot")
-            return True
-        else:
+        if self.__mBallShootTopLeftPosition is None or self.__mBallShootTopLeftPosition == 0:
             print("Don't find out ball shoot")
             return False
+        else:
+            print("Find out ball shoot")
+            return True
 
     def isWaterFall(self):
-        imageWaterfall = cv2.imread('./imageTarget/waterfall.png')
-        if imageWaterfall is None:
-            print("Can't open waterfall image")
-            return False
-        res = self.checkImageTarget(imageWaterfall)
-        if res[0] == True:
-            print("Find out waterfall")
-            return True
-        else:
+        if self.__mWaterfallTopLeftPosition is None or self.__mWaterfallTopLeftPosition == 0:
             print("Don't find out waterfall")
             return False
+        else:
+            print("Find out waterfall")
+            return True
 
     def isShooter(self):
-        res = self.checkImageTarget(self.__mImageShooter)
-        if res[0] == True:
-            self.__mShooterTopLeftPosition = res[1]
-            return True
-        else:
-            print("Don't find Shoot")
+        if self.__mShooterTopLeftPosition is None or self.__mShooterTopLeftPosition == 0:
+            print("Don't find Shooter")
             return False
+        else:
+            print("Find out Shooter")
+            return True
 
     def getPositionShooter(self):
-        x = self.__mShooterTopLeftPosition[0] + self.__mWidthShooter *0.5
-        y = self.__mHeithScreenLaptop - (self.__mShooterTopLeftPosition[1] + self.__mHeightShooter - 10)
+        x = self.__mShooterTopLeftPosition[0] + self.__mWidthShooter * 0.5 + 10
+        y = self.__mHeithScreenLaptop - (self.__mShooterTopLeftPosition[1] + self.__mHeightShooter - 35)
         posShooter = Coordinate(x, y)
         return posShooter
 
     def isPersonShoot(self):
-        res = self.checkImageTarget(self.__mImagePersonShoot)
-        if res[0] == True:
-            self.__mPersonTopLeftPosition = res[1]
-            return True
-        else:
-            print("Don't find Person shot")
+        if self.__mPersonTopLeftPosition is None or self.__mPersonTopLeftPosition == 0:
+            print("Don't find Person shoot")
             return False
+        else:
+            return True
 
     def getPositionPersonShoot(self, headshot : bool):
         x = 0
         y = 0
         if headshot == True:
             x = round(self.__mPersonTopLeftPosition[0] + self.__mWidthPersonShot *0.5)
-            y = self.__mHeithScreenLaptop - (self.__mPersonTopLeftPosition[1] + 15)
+            y = self.__mHeithScreenLaptop - (self.__mPersonTopLeftPosition[1] + 10)
         else:
             x = round(self.__mPersonTopLeftPosition[0] + self.__mWidthPersonShot *0.5)
-            y = self.__mHeithScreenLaptop - (self.__mPersonTopLeftPosition[1] + 60)
+            y = self.__mHeithScreenLaptop - (self.__mPersonTopLeftPosition[1] + 45)
         posShooter = Coordinate(x, y)
         return posShooter
 
@@ -135,36 +124,87 @@ class ProcessImage:
         self.__mImageInput = cv2.cvtColor(screenshot_np, cv2.COLOR_RGB2BGR)
         assert self.__mImageInput is not None,  f"Input image is invalue. Please double check!!!"
 
-    def drawParabol(self, angle, vo: int, g: int, height : int, width : int):
+        maxLoc = self.checkImageTarget(self.__mImageShooter)
+        if  maxLoc != 0:
+            self.__mShooterTopLeftPosition = maxLoc
+
+        maxLoc = self.checkImageTarget(self.__mImagePersonShoot)
+        if  maxLoc != 0:
+            self.__mPersonTopLeftPosition = maxLoc
+
+        self.__imageShield = cv2.imread('./imageTarget/shield.png')
+        if self.__imageShield is None:
+            print("Can't open shield image")
+            return False
+        else:
+            maxLoc = self.checkImageTarget(self.__imageShield)
+            if  maxLoc != 0:
+                self.__mShieldTopLeftPosition = maxLoc
+
+        self.__imageBall = cv2.imread('./imageTarget/ball.png')
+        if self.__imageBall is None:
+            print("Can't open ball image")
+            return False
+        else:
+            maxLoc = self.checkImageTarget(self.__imageBall)
+            if  maxLoc != 0:
+                self.__mBallTopLeftPosition = maxLoc
+
+        self.__imageWaterfall = cv2.imread('./imageTarget/waterfall.png')
+        if self.__imageWaterfall is None:
+            print("Can't open waterfall image")
+            return False
+        else:
+            maxLoc = self.checkImageTarget(self.__imageWaterfall)
+            if  maxLoc != 0:
+                self.__mWaterfallTopLeftPosition = maxLoc
+
+        if self.isShooter() and self.isPersonShoot():
+            # self.showImage(self.__mImageCrop)
+            return True
+        else:
+            return False
+
+    def detectObject(self, position, sizeImage, color):
+        bottom_right = (position[0] + sizeImage.shape[1], position[1] + sizeImage.shape[0])
+        cv2.rectangle(self.__mImageInput, position, bottom_right, color, 2)
+
+    def drawObject(self, angle, vo: int, g: int, height : int, width : int):
         # Iterate through all x values ​​from 0 to width to calculate y
         pos = self.getPositionShooter()
         x = int(pos.x)
         y = pos.y
-        print("x ", x)
-        print("y ", y)
-        print("width ", width)
-        print("height ", height)
-        print("angle ", angle)
-        print("Vo ", vo)
-        print("g ", g)
-        print("tan ", math.tan(math.radians(angle)))
-        previous_point = None
+        previousPoint = None
         radianAngle = math.radians(angle)
+        blue = (255, 0, 0)
+        red = (0, 0, 255)
+        green = (0, 255, 0)
         for Xo in range(width):
             Yo = int((math.tan(radianAngle) * Xo) + (-0.5 * g * (1 + pow(math.tan(radianAngle), 2)) * pow(Xo, 2))/pow(vo, 2))
             if 0 <= Yo < height:
-                current_point = (Xo + x, 1080 - (Yo + y))
-                # print("current_point ", current_point)
+                currentPoint = (Xo + x, self.__mHeithScreenLaptop - (Yo + y))
                 # Draw a straight line from the previous point to the current point to create a curve
-                green = (0, 255, 0)
-                if previous_point is not None:
-                    cv2.line(self.__mImageInput, previous_point, current_point, green, 2)
-                previous_point = current_point
-                current_point = (Xo + x, (Yo + y))
-                print("current_point ", current_point)
-        cv2.imshow("Parabola on Image", self.__mImageInput)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+                if previousPoint is not None:
+                    cv2.line(self.__mImageInput, previousPoint, currentPoint, green, 2)
+                previousPoint = currentPoint
+
+        if self.isShooter():
+            self.detectObject(self.__mShooterTopLeftPosition, self.__mImageShooter, red)
+
+        if self.isPersonShoot():
+            self.detectObject(self.__mPersonTopLeftPosition, self.__mImagePersonShoot, red)
+
+        if self.isShield():
+            self.detectObject(self.__mShieldTopLeftPosition, self.__imageShield, blue)
+
+        if self.isBall():
+            self.detectObject(self.__mBallTopLeftPosition, self.__imageBall, blue)
+
+        if self.isWaterFall():
+            self.detectObject(self.__mWaterfallTopLeftPosition, self.__imageWaterfall, blue)
+
+        # self.showImage(self.__mImageInput)
+        self.storeImage(self.__mImageInput)
 
 # test  = ProcessImage()
 # screenshot = pyautogui.screenshot()
--- main.py
+++ main.py
@@ -8,45 +8,39 @@ import pyautogui
 import time
 import sys
 
-# mouse = Controller()
-# position = mouse.position
-# x, y = position
-# print(f"Current mouse position is x = {x} y = {y}")
-
 def main():
     image = ProcessImage()
     screenshot = pyautogui.screenshot()
     personShot = Coordinate(0, 0)
-    image.inputImage(screenshot)
-    if image.isPersonShoot():
-        if image.isShield():
-            personShot = image.getPositionPersonShoot(True)
-        else:
-            personShot = image.getPositionPersonShoot(False)
+    if image.inputImage(screenshot) == False:
+        return
+
+    if image.isShield() and image.isPersonShoot():
+        personShot = image.getPositionPersonShoot(True)
     else:
-        return
-    if image.isShooter() == False:
-        return
+        personShot = image.getPositionPersonShoot(False)
+    shoot = ShootAngle(image.getPositionShooter(), personShot)
+
+    # print(shoot)
+    image.drawObject(shoot.angle, ShootAngle.Vo(), ShootAngle.g(), int(shoot.point1and2.deltaPointY), int(shoot.point1and2.deltaPointX))
+
     if image.isWaterFall():
         exit()
-    shoot = ShootAngle(image.getPositionShooter(), personShot)
-    print(shoot)
-    image.drawParabol(shoot.angle, ShootAngle.Vo(), ShootAngle.g(), int(shoot.point1and2.deltaPointY), int(shoot.point1and2.deltaPointX))
-
-    # click = ClickMouse(image.getPositionShooter())
-    # click.timeShooting = shoot.convertAngleToTime
-    # if image.isBall():
-    #     for i in range(10):
-    #         screenshot = pyautogui.screenshot()
-    #         image.inputImage(screenshot)
-    #         if image.isBallShoot():
-    #             time.sleep(0.1)
-    #             click.shooting
-    #             break
-    #         time.sleep(0.1)
-    # else:
-    #     click.shooting
-    # time.sleep(2)
+    click = ClickMouse(image.getPositionShooter())
+    click.timeShooting = shoot.convertAngleToTime
+    if image.isBall():
+        exit()
+        # for i in range(10):
+        #     screenshot = pyautogui.screenshot()
+        #     image.inputImage(screenshot)
+        #     if image.isBallShoot():
+        #         time.sleep(0.1)
+        #         click.shooting
+        #         break
+        #     time.sleep(0.1)
+    else:
+        click.shooting
+    time.sleep(2)
 
 if __name__ == "__main__":
     # print(sys.argv[1])
@@ -55,4 +49,5 @@ if __name__ == "__main__":
             main()
     else:
         for i in range(int(sys.argv[1])):
+            print(f"------------------Time {i}----------------------")
             main()
